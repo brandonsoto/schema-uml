6e64d9523a8c1919493c1858418674158bf8ee63
Brandon Soto <brandon.soto09@gmail.com>
2017-08-02 22:42:18 -0700

    Create UML for example protos

--- protobuf2uml/make_uml.sh
+++ protobuf2uml/make_uml.sh
@@ -1,37 +1,22 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Author: Malisa Smith
+PROTO_DIR=$PWD/proto
+PROTO_OUT_DIR=$PWD/proto_out
+FILE_DESCRIPTOR_SET=$PROTO_OUT_DIR/MyFileDescriptorSet.pb
+PYTHONPATH=$PWD:$PROTO_OUT_DIR
 
-# Download all the proto schema files into the schemas_proto folder
-# First clean-up old files from previous runs
-rm -rf schemas_proto/*
-# Obtain the raw github url's if not raw already:
-raw_schema_urls=$(python url_converter.py --getrawfromfile schema_urls)
-for raw_url in ${raw_schema_urls};
-do
-    wget --timestamping --directory-prefix ./schemas_proto ${raw_url};
-done
-
-# Replace user-defined package imports with no path. This allows proto files to find each other.
-#For example,     import "ga4gh/common.proto";       becomes       import "common.proto";
-for proto_file in schemas_proto/*; do
-    sed -e 's:ga4gh/::g' $proto_file > $proto_file.tmp && mv $proto_file.tmp $proto_file
-    # sed -i '' s/ga4gh\///g $proto_file
-done
-
-# Remove any temporary files in the schemas_proto directory which have have been created as a result of editing, etc:
-#rm -rf schemas_proto/*~
+mkdir -p $PROTO_OUT_DIR
 
 # Generate descriptor_pb2.py with protoc:
-protoc descriptor.proto --python_out=.
+protoc -I=$PWD --python_out=$PROTO_OUT_DIR $PWD/descriptor.proto
 
 # convert .proto files into a serialized FileDescriptorSet for input into descriptor2uml.py
-cd schemas_proto
-protoc --include_source_info -o MyFileDescriptorSet.pb *
-cd ../
+protoc -I=$PROTO_DIR --include_source_info -o $FILE_DESCRIPTOR_SET $PROTO_DIR/messages.proto $PROTO_DIR/messages2.proto
+
+export PYTHONPATH=$PYTHONPATH
 
 # Make the dot file which describes the UML diagram. The type_header_comments file can be empty (or you can remove the option altogether)
-python descriptor2uml.py --descriptor ./schemas_proto/MyFileDescriptorSet.pb --dot uml.dot --urls schema_urls #--type_comments type_header_comments 
+python descriptor2uml.py --descriptor $FILE_DESCRIPTOR_SET --dot uml.dot # --urls schema_urls #--type_comments type_header_comments
 
 # Finally, draw the UMl diagram
 dot uml.dot -T svg -o uml.svg
